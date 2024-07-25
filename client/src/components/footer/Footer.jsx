1fd97487294d3b44f56d29021b8b5d623313f58f
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2024-07-25 08:41:04 +0100

    components: footerText - styled and responsive

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -11,9 +11,9 @@ const Footer = () => {
     <footer className="footer__container">
 
         {/* heading */}
-        <h1 className="footer__heading">
+        <h2 className="footer__heading">
             Get in Touch
-        </h1>
+        </h2>
 
         {/* socials */}
         <div className="footer__socials">
@@ -29,55 +29,6 @@ const Footer = () => {
         </div>
 
         <FooterText/>
-
-        {/* text
-        <div className="footer__textContainer">
-            <section className="footer__text">
-                <h4 className="footer__name">
-                    Cam Adams
-                    <br/>
-                    The Bees Knees Management
-                </h4>
-                <h5 className="footer__address">
-                    503 Broadway Penthouse,
-                    <br/>
-                    New York, NY 10012, USA
-                </h5>
-                <h5 className="footer__email">
-                    info@thebeesknees.com
-                </h5>
-            </section>
-            <section className="footer__text">
-                <h4 className="footer__name">
-                    Pearl Gregg
-                    <br/>
-                    Limitless Artist Group
-                </h4>
-                <h5 className="footer__address">
-                    Booking Agent for
-                    <br/>
-                    US / South America / Japan
-                </h5>
-                <h5 className="footer__email">
-                    pearl.gregg@limitlessag.com
-                </h5>
-            </section>
-            <section className="footer__text">
-                <h4 className="footer__name">
-                    Carson Whyte
-                    <br/>
-                    ARCH Entertainment
-                </h4>
-                <h5 className="footer__address">
-                    Booking Agent for
-                    <br/>
-                    UK / EU / AU
-                </h5>
-                <h5 className="footer__email">
-                    cwhyte@archentertainment.com
-                </h5>
-            </section>
-        </div> */}
 
         {/* logo container */}
         <div className="footer__logoContainer">
