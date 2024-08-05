521904fd4b25e2caf2a9e43e0aed0996415c9e10
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2024-08-05 20:45:15 +0100

    components: copyrightText - styled and responsive

---
+++ client/src/components/copyrightText/CopyrightText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './CopyrightText.scss'
+
+const CopyrightText = () => {
+  return (
+    <h5 className="copyrightText__text">
+        Copyright The Bees Knees 2021 All Rights Reserved
+    </h5>
+  )
+}
+
+export default CopyrightText
--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Logo from '../../assets/logo/logo-bandsite.svg'
-import FooterText from '../footerText/FooterText'
 import FooterTop from '../footerTop/FooterTop'
+import FooterText from '../footerText/FooterText'
+import CopyrightText from '../copyrightText/CopyrightText'
 import './Footer.scss'
 
 const Footer = () => {
@@ -13,9 +14,7 @@ const Footer = () => {
         {/* logo container */}
         <div className="footer__logoContainer">
             <img src={Logo} alt="logo" className="footer__bottomLogo" />
-            <h5 className="footer__copyrightText">
-                Copyright The Bees Knees 2021 All Rights Reserved
-            </h5>            
+            <CopyrightText/>
         </div>
         
     </footer>
