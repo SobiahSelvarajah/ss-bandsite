a59a5895919068f53a8d4deab4948b2cc699520d
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2024-09-29 23:03:23 +0100

    components: bioHero html coded

---
+++ client/src/components/bioHero/BioHero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './BioHero.css'
+
+const BioHero = () => {
+  return (
+    <div className="bioHero__container">
+        <section className="bioHero__text">
+            <h1 className="bioHero__heading">
+                The Bees Knees
+            </h1>            
+        </section>
+    </div>
+  )
+}
+
+export default BioHero
--- client/src/pages/biography/Biography.jsx
+++ client/src/pages/biography/Biography.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import BioHero from '../../components/bioHero/BioHero'
 import './Biography.css'
 
 const Biography = () => {
   return (
-    <div>Bio</div>
+    <div className="biography__container">
+      <BioHero/>
+    </div>
   )
 }
 
