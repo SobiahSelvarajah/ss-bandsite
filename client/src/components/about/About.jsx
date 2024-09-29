9fde0ccc2f3538066c9d0437557f4177c36ef36f
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2024-09-29 23:14:25 +0100

    components: about set up

---
+++ client/src/components/about/About.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './About.css'
+
+const About = () => {
+  return (
+    <article className="about__container">
+        {/* heading */}
+        {/* image */}
+        {/* comment */}
+        {/* description */}
+    </article>
+  )
+}
+
+export default About
