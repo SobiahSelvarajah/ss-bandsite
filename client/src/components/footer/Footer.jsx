0fb920793055637094021666c89a46901b3f23e4
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2024-09-29 22:24:16 +0100

    components: footerText html coded

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import FooterTop from '../footerTop/FooterTop'
+import FooterText from '../footerText/FooterText'
 import './Footer.css'
 
 const Footer = () => {
   return (
     <footer className="footer__container">
         <FooterTop/>
+        <FooterText/>
     </footer>
   )
 }
