d81ce4b116f6a35970f2264a22ab26dbe099b338
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2024-09-30 07:47:56 +0100

    components: aboutComment html coded

---
+++ client/src/components/aboutComment/AboutComment.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './AboutComment.css'
+
+const AboutComment = () => {
+  return (
+    <section className="aboutComment__container">
+        <h3 className="aboutComment__text">
+            “We push each other to be the best. It's not uncommon for one of us to say 'this needs to be better, back to the drawing board'”
+        </h3>
+        <h4 className="aboutComment__name">
+            - Bryan Teddy, lead vocalist of The Bees Knees
+        </h4>
+    </section>
+  )
+}
+
+export default AboutComment
--- client/src/pages/biography/Biography.jsx
+++ client/src/pages/biography/Biography.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import BioHero from '../../components/bioHero/BioHero'
+import About from '../../components/about/About'
 import './Biography.css'
 
 const Biography = () => {
   return (
     <div className="biography__container">
       <BioHero/>
+      <About/>
     </div>
   )
 }
