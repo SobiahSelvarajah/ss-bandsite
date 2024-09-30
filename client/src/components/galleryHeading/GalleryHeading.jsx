ebf0a1733f782e2507d488c07f9c66b6b041bff2
Sobiah Selvarajah <sobiahselvarajah@hotmail.co.uk>
2024-09-30 08:27:01 +0100

    components: galleryHeading html coded

---
+++ client/src/components/galleryHeading/GalleryHeading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './GalleryHeading.css'
+
+const GalleryHeading = () => {
+  return (
+    <h2 className="galleryHeading__heading">
+        Photo Gallery
+    </h2>
+  )
+}
+
+export default GalleryHeading
--- client/src/pages/biography/Biography.jsx
+++ client/src/pages/biography/Biography.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import BioHero from '../../components/bioHero/BioHero'
 import About from '../../components/about/About'
+import Gallery from '../../components/gallery/Gallery'
 import './Biography.css'
 
 const Biography = () => {
@@ -8,6 +9,7 @@ const Biography = () => {
     <div className="biography__container">
       <BioHero/>
       <About/>
+      <Gallery/>
     </div>
   )
 }
